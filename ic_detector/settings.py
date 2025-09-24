b1c7c03123d5a8677918afa8ed5ab1747c2e7f19
Meet Dixit <infomeet06@gmail.com>
2025-09-25 00:24:49 +0530

    SIH2025_ICsdetector_V0

--- ic_detector/settings.py
+++ ic_detector/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2a3q4@4(f8w-$h1v6w^2=z+8oknqfip#=k#!0a-%vwkma4&if$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
 
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,4 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'predictor', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'predictor/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
